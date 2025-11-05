eef15ee4968d8b13220efa82802e52b3e3451a23
Mahmoud <mahmoudabbas393@gmail.com>
2025-11-05 14:34:47 +0200

    feat: update notification routing and clean up unused service functions

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -54,19 +54,63 @@ self.addEventListener("notificationclick", (event) => {
   const data = event.notification.data || {};
   let url = "/";
 
-  // 🔀 Route based on type
   switch (data.notification_type) {
     case "wallet":
-      url = `/wallet/${data.order_id}`;
+      url = `/my-profile/my-wallet`;
       break;
-    case "order":
-      url = `/orders/${data.order_id}`;
+    case "package":
+      url = `/my-platform`;
       break;
-    case "chat":
-      url = `/chat/${data.chat_id}`;
+    case "offer_accepted":
+      url = `/my-works/${data?.operation_id}`;
+      break;
+    case "contract_request":
+      url = `/my-contracts/${data?.operation_id}`;
+      break;
+    case "help_service ":
+      url = `/offers/${data?.operation_id}`;
+      break;
+    case "goal":
+      url = `/goal/${data?.operation_id}`;
+      break;
+    case "follow":
+      url = `/my-audience`;
+      break;
+    case "community_new_member":
+      url = `/my-audience`;
+      break;
+    case "consultation":
+      url = `/consultaion-details/${data.operation_id}`;
+      break;
+    case "inquiry":
+      url = `/notifications?tab=inquries`;
+      break;
+    case "meeting":
+      url = `my-community/meetings`;
+      break;
+    case "post":
+      url = `/posts/${data.operation_id}`;
+      break;
+    case "comment":
+      url = `/posts/${data.operation_id}`;
+      break;
+    case "offer":
+      url = `/goal/${data?.operation_id}`;
+      break;
+    case "work":
+      url = `/goal/${data?.operation_id}`;
+      break;
+    case "general":
+      url = `/notifications`;
+      break;
+    case "community_chat":
+      url = `/community/${data.operation_id}/chats/`;
+      break;
+    case "group_chat":
+      url = `/chat/${data.operation_id}`;
       break;
     default:
-      url = "/";
+      url = "/ ";
   }
 
   event.waitUntil(
