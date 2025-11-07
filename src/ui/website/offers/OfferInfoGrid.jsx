e63b6fb31caa41c126e9d85b74da6cbb0b02fb7e
Mahmoud <mahmoudabbas393@gmail.com>
2025-11-07 16:42:44 +0200

    fix : fix  helper card in the assistance details styles in mobile screens

--- src/ui/website/offers/OfferInfoGrid.jsx
+++ src/ui/website/offers/OfferInfoGrid.jsx
@@ -4,36 +4,36 @@ export default function OfferInfoGrid({ offer }) {
   const { t } = useTranslation();
   return (
     <div className="info-grid">
-      <div className="info-box">
+      <div className="info-box info-box-grow-min-width ">
         <div className="label">{t("website.offerDetails.field")}</div>
         <div className="value">{offer.category_title}</div>
       </div>
 
-      <div className="info-box">
+      <div className="info-box info-box-grow-min-width">
         <div className="label">{t("website.offerDetails.specialty")}</div>
         <div className="value">{offer.sub_category_title}</div>
       </div>
 
-      <div className="info-box">
+      <div className="info-box info-box-grow-min-width">
         <div className="label">{t("website.offerDetails.helpValue")}</div>
         <div className="value">
           {offer.help_service.price} <img src="/icons/ryal.svg" alt="ريال" />
         </div>
       </div>
 
-      <div className="info-box">
-        <div className="label">{t("website.offerDetails.preferredGender")}</div>
+      <div className="info-box info-box-grow-min-width">
+        <div className="label">{t("beneficiaryIdentityPreference")}</div>
         <div className="value">{offer.preferred_gender}</div>
       </div>
 
-      <div className="info-box">
+      <div className="info-box info-box-grow-min-width">
         <div className="label">{t("website.offerDetails.ageRange")}</div>
         <div className="value">
           {offer.help_service.from_age} - {offer.help_service.to_age}
         </div>
       </div>
 
-      <div className="info-box">
+      <div className="info-box info-box-grow-min-width ">
         <div className="label">{t("website.offerDetails.duration")}</div>
         <div className="value">
           {offer.help_service.duration}{" "}
