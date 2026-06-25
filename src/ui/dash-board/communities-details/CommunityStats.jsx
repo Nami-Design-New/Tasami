ea269df6735cec8950243930c36ebbf22e5e1ddb
Mahmoud <mahmoudabbas393@gmail.com>
2026-06-25 15:34:58 +0300

    fix the scss and css files styls conflicts

--- src/ui/dash-board/communities-details/CommunityStats.jsx
+++ src/ui/dash-board/communities-details/CommunityStats.jsx
@@ -1,12 +1,18 @@
 import Currency from "../../Currency";
 import { CommunityStat } from "./CommunityStat";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router";
 import groupSecond from "../../../assets/icons/group-second.svg";
 import heartFill from "../../../assets/icons/heart-fill.svg";
 import walletSecond from "../../../assets/icons/wallet-second.svg";
 import activeIndex from "../../../assets/icons/active-index.svg";
 export default function CommunityStats({ community }) {
   const { t } = useTranslation();
+  const communityOwnerId =
+    community?.helper?.id ||
+    community?.user?.id ||
+    community?.user_id ||
+    community?.helper_id;
 
   return (
     <div className="community-stats">
@@ -15,6 +21,17 @@ export default function CommunityStats({ community }) {
         icon={groupSecond}
         value={community?.members_count || community?.user_count || 0}
         label={t("member")}
+        action={
+          community?.is_subscribed === true &&
+          communityOwnerId && (
+            <Link
+              to={`/community/${community?.id}/members?user_id=${communityOwnerId}`}
+              className="community-stat-action"
+            >
+              {t("community.membersList")}
+            </Link>
+          )
+        }
       />
       <CommunityStat
         title={t("likes")}
