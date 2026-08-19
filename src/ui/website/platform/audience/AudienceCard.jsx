c04249236769546673d9213dac96a9cffe470915
Mahmoud <mahmoudabbas393@gmail.com>
2026-08-19 13:50:58 +0200

    fix(869ekdf6k): unify user identity cards

--- src/ui/website/platform/audience/AudienceCard.jsx
+++ src/ui/website/platform/audience/AudienceCard.jsx
@@ -1,48 +1,12 @@
-import { useTranslation } from "react-i18next";
-import { Link } from "react-router";
-import medalIcon from "../../../../assets/icons/medal.svg";
-export default function AudienceCard({ data, date = false }) {
-  const { t } = useTranslation();
+import HelperCard from "../../../cards/HelperCard";
 
+export default function AudienceCard({ data }) {
   return (
-    <Link to={`/helper/${data?.user?.id}`} className="audience-card">
-      <div
-        className={
-          data?.user?.is_online
-            ? "user-image-wrapper"
-            : "user-image-wrapper offline"
-        }
-      >
-        <img
-          className="user-image"
-          src={data?.user?.image || "/images/profile image.svg"}
-          alt="profile photo"
-        />
-      </div>
-      <div className="info">
-        <h2>
-          <span>{data?.user?.name}</span>
-          <span className="score">
-            <img src={medalIcon} />
-            {data?.user?.experience_level}
-          </span>
-        </h2>
-
-        <div className="country-date">
-          {data?.user?.country && (
-            <p className="country">
-              <img src={data?.user?.country?.image} />
-              <span>{data?.user?.country?.title}</span>
-            </p>
-          )}
-          {date && data?.created_at && (
-            <p className="date">
-              {t("website.platform.audience.memberSince")}{" "}
-              {data?.created_at}{" "}
-            </p>
-          )}
-        </div>
-      </div>
-    </Link>
+    <HelperCard
+      helper={{
+        ...data.user,
+        image: data.user.image || "/images/profile image.svg",
+      }}
+    />
   );
 }
